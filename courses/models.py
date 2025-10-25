205474f9333ef5f31143d5340414e4bea4c24ced
ruldak <ruldak3306@gmail.com>
2025-10-25 17:12:23 +0700

    Changed on_delete=models.CASCADE in Course models to on_delete=models.SET_NULL and null=True to prevent accidental data loss.

--- courses/models.py
+++ courses/models.py
@@ -14,7 +14,7 @@ class Category(models.Model):
 class Course(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    instructor = models.ForeignKey(User, on_delete=models.CASCADE)
+    instructor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
