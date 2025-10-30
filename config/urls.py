af734dd40c9f8aa4fe490487f81011a0b9421b39
ruldak <ruldak3306@gmail.com>
2025-10-30 21:05:27 +0700

    add endpoint for verify token

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
@@ -24,6 +25,7 @@ urlpatterns = [
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
      # GET endpoints
     path('api/categories/', views.CategoryListView.as_view(), name='category-list'),
